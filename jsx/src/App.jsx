1db51cea3a493e69291304f7a336ede2b7fb54ee
Rampravesh <rampraveshkr4545@gmail.com>
2025-09-17 04:17:01 +0530

    jsx  complete

--- jsx/src/App.jsx
+++ jsx/src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import React from "react";
 
 function App() {
   let name = "rampravesh kumar";
@@ -32,9 +30,7 @@ function App() {
       </h1> */}
       {/* <h1>{age > 18 ? "you can drive" : "you can not drie"}</h1> */}
       {/* <h1>{console.log(greet())}</h1> */}
-
       {/* inline style */}
-
       {/* <h1
         style={{
           backgroundColor: "red",
@@ -51,14 +47,26 @@ function App() {
         )}
       </div> */}
       {/* {true && <p>if true then run </p>} */}
-
-      <div>
+      {/* <div>
         <ul>
           {arr.map((item) => (
             <li key={item}>{item}</li>
           ))}
         </ul>
-      </div>
+      </div> */}
+      {/* <div>{React.createElement("h1", null, "hello react create element")}</div> */}
+      {/* {React.createElement(
+        "h1",
+        { id: "heading", style: { color: "red" } },
+        "hello react create element"
+      )} */}
+      {/* {React.createElement(
+        "a",
+        { href: "https://www.youtube.com/" },
+        "go to youtube"
+      )} */}
+
+      {/* {React.createElement("h1", { className: "heading" }, `hello ${name}`)} */}
     </>
   );
 }
