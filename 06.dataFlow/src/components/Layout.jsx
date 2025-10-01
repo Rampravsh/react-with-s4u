db1d65e1fce558159d69ef96650698d94c4cbfe8
Rampravesh <rampraveshkr4545@gmail.com>
2025-10-01 06:30:00 +0530

    feat: add Header, Footer, Layout, Hero, and About components with Tailwind CSS styling

--- 06.dataFlow/src/components/Layout.jsx
+++ 06.dataFlow/src/components/Layout.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import Hero from "./layout/Hero";
+import About from "./layout/About";
 
 const Layout = () => {
   return (
-    <div>Layout</div>
-  )
-}
+    <div className="h-screen text-center bg-gray-200 text-black ">
+      Layout
+      <Hero />
+      <About />
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
---
+++ 06.dataFlow/src/components/layout/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='bg-gray-400 mt-2'>About</div>
+  )
+}
+
+export default About
