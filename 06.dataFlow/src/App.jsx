783dd8bc1150711676ed826c0f16f4efadf733b3
Rampravesh <rampraveshkr4545@gmail.com>
2025-10-01 06:07:43 +0530

    refactor: restructure project for data flow and integrate Tailwind CSS
    - Updated package.json to rename project and add Tailwind CSS dependencies.
    - Removed App.css and associated styles, transitioning to Tailwind CSS for styling.
    - Simplified App component by removing unused components and integrating new Header, Layout, and Footer components.
    - Deleted unused Button and Comp components to streamline codebase.
    - Created new components: Footer, Header, and Layout for better structure.
    - Updated index.css to include Tailwind CSS and simplified body styles.
    - Adjusted main.jsx to remove unnecessary CSS import.
    - Modified vite.config.js to include Tailwind CSS plugin.

--- 06.dataFlow/src/App.jsx
+++ 06.dataFlow/src/App.jsx
@@ -1,25 +1,14 @@
 import React from "react";
-import Layout from "./Layout";
-import "./App.css";
-import Button from "./Button";
-import Comp from "./Comp";
+import Footer from "./components/Footer";
+import Headers from "./components/Header";
+import Layout from "./components/Layout";
 
 const App = () => {
-  const handleClick = () => {
-    console.log("Button Clicked");
-  };
   return (
-    <div>
-      {/* <Layout>
-        <div>Content goes here</div>
-        <footer>Footer goes here</footer>
-      </Layout>
-      <Button label={<h1>Click Me</h1>} />
-      <Button onClick={handleClick}>
-        <h1>Click Me</h1>
-      </Button> */}
-      <Comp age={30} />
-      <Comp name="Jane Smith" />
+    <div className="text-7xl">
+      <Headers />
+      <Layout />
+      <Footer />
     </div>
   );
 };
