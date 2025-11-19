d0be4f32b4e943c9fd1c3bf1031fa49587f76a62
Rampravesh <rampraveshkr4545@gmail.com>
2025-11-19 17:24:14 +0530

    some error resolve

--- 15.Select Dropdown/src/DependentSelect.jsx
+++ 15.Select Dropdown/src/DependentSelect.jsx
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import data from "./assets/data";
 
 const DependentSelect = () => {
-  const [country, setCountry] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState("");
+  const [selectedState, setSelectedState] = useState("");
   //   console.log(Object.keys(data));
-  console.log(country);
+  console.log(selectedCountry, selectedState);
   return (
     <>
       <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
@@ -21,7 +22,11 @@ const DependentSelect = () => {
               <select
                 id="country-select"
                 name="country-select"
-                onChange={(e) => setCountry(data[e.target.value])}
+                value={selectedCountry}
+                onChange={(e) => {
+                  setSelectedState("");
+                  setSelectedCountry(e.target.value);
+                }}
                 className="block appearance-none w-full bg-white border border-gray-300 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-blue-500 shadow-sm"
               >
                 <option value="">---Select Country---</option>
@@ -46,41 +51,43 @@ const DependentSelect = () => {
           </div>
 
           {/* State Select */}
-          <div>
-            <label
-              htmlFor="state-select"
-              className="block text-gray-700 text-sm font-bold mb-2"
-            >
-              Select State:
-            </label>
-            <div className="relative">
-              <select
-                id="state-select"
-                name="state-select"
-                className="block appearance-none w-full bg-white border border-gray-300 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-blue-500 shadow-sm"
+          {selectedCountry && (
+            <div>
+              <label
+                htmlFor="state-select"
+                className="block text-gray-700 text-sm font-bold mb-2"
               >
-                <option value="">---Select State---</option>
-                {country.length > 0
-                  ? country.map((state) => {
-                      return (
-                        <option key={state} value={state}>
-                          {state}
-                        </option>
-                      );
-                    })
-                  : null}
-              </select>
-              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                <svg
-                  className="fill-current h-4 w-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
+                Select State:
+              </label>
+              <div className="relative">
+                <select
+                  id="state-select"
+                  name="state-select"
+                  value={selectedState}
+                  onChange={(e) => setSelectedState(e.target.value)}
+                  className="block appearance-none w-full bg-white border border-gray-300 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-blue-500 shadow-sm"
                 >
-                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                </svg>
+                  <option value="">---Select State---</option>
+                  {data[selectedCountry].map((state) => {
+                    return (
+                      <option key={state} value={state}>
+                        {state}
+                      </option>
+                    );
+                  })}
+                </select>
+                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                  <svg
+                    className="fill-current h-4 w-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                  </svg>
+                </div>
               </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
     </>
