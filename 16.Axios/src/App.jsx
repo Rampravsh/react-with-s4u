068925b22016c7494af7ad96d9bea4448108d768
Rampravesh <rampraveshkr4545@gmail.com>
2025-12-03 20:58:26 +0530

    api call by fetch

--- 16.Axios/src/App.jsx
+++ 16.Axios/src/App.jsx
@@ -1,7 +1,25 @@
+import axios from "axios";
+
 function App() {
+  // const fetchData = async () => {
+  //   const res = await fetch("http://jsonplaceholder.typicode.com/users");
+  //   const data = await res.json();
+  //   console.log(data);
+  // };
+
+  const fetchData = async () => {
+    const data = await axios.get("http://jsonplaceholder.typicode.com/users");
+    console.log(data);
+  };
   return (
     <>
       <h1>project setup</h1>
+      <button
+        onClick={fetchData}
+        className="bg-amber-300 p-4 rounded-2xl mt-4 hover:bg-amber-400"
+      >
+        Fetch data on click
+      </button>
     </>
   );
 }
