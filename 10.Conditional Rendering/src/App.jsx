8a4e838c67ff8ace19c5a6920d2842aa0df841b4
Rampravesh <rampraveshkr4545@gmail.com>
2026-02-09 15:31:09 +0530

    revision

--- 10.Conditional Rendering/src/App.jsx
+++ 10.Conditional Rendering/src/App.jsx
@@ -1,10 +1,44 @@
 import "./App.css";
+import A from "./component/A.jsx";
+import B from "./component/B.jsx";
+import C from "./component/C.jsx";
 import ConditionalRenderingExamples from "./examples.jsx";
 
 function App() {
+  // const name="rampravesh kumar"
+
+  // const opntion = "a";
+
+  // let content = <h1>Hello how are you</h1>;
+
+  // if (opntion === "a") {
+  //   content = <A />;
+  // } else if (opntion === "b") {
+  //   content = <B />;
+  // } else {
+  //   content = <C />;
+  // }
+
+  const color = "red";
+
   return (
     <div>
-      <ConditionalRenderingExamples />
+      <h1 style={{ backgroundColor: color === "red" ? "red" : "blue" }}>
+        Hello how are you
+      </h1>
+      {/* <h1 className={{backgroundColor: 'blue'}}>hii </h1> */}
+
+      {/* <div>{content}</div> */}
+
+      {/* <ConditionalRenderingExamples /> */}
+
+      {/* {
+        name?<h1>{name}</h1>:null
+      } */}
+
+      {/* {
+        name && <h1> {name} </h1> && <h1>sabse last bala dikhega && eske bad </h1>
+      } */}
     </div>
   );
 }
