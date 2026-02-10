1ff8a6602236e972a5d5e11f6916d3f4400cb1e4
Rampravesh <rampraveshkr4545@gmail.com>
2026-02-10 10:41:23 +0530

    use ref to focus input field by there current properties

--- 17.Useref/src/App.jsx
+++ 17.Useref/src/App.jsx
@@ -1,7 +1,17 @@
+import { useRef } from "react";
+
 function App() {
+  const inputRef = useRef();
+  const handleFocus = () => {
+    inputRef.current.focus()
+    console.log(inputRef);
+  };
   return (
     <>
-      <h1>project setup</h1>
+      <div className="flex justify-center items-center min-h-screen">
+        <input type="text" placeholder="type something" ref={inputRef}/>
+        <button className="p-1 bg-amber-200 rounded-2xl ml-2 cursor-pointer" onClick={handleFocus}>Focus</button>
+      </div>
     </>
   );
 }
