3906c8c9000a60a982e2d91711d48ee1bf748583
Rampravesh <rampraveshkr4545@gmail.com>
2026-02-10 11:57:14 +0530

    ho gya use Ref clear

--- 17.Useref/src/App.jsx
+++ 17.Useref/src/App.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const App = () => {
-  return <div>App</div>;
+  const [randomNumber, setrandomNumber] = useState(0);
+  // const [renderCount, setrenderCount] = useState(0);
+  const renderCountRef = useRef(0);
+
+  const generateRandomNumber = () => {
+    const no = Math.random();
+    setrandomNumber(no);
+  };
+
+  useEffect(() => {
+    // setrenderCount(renderCount + 1);
+    console.log("re render hua be");
+    console.log(renderCountRef.current);
+    renderCountRef.current += 1;
+  });
+
+  return (
+    <div>
+      <h1>{randomNumber}</h1>
+      <h1>Re-render Count:{renderCountRef.current} </h1>
+      {/* <h1>Re-render Count: {renderCount} </h1> */}
+      <button onClick={generateRandomNumber}>Generate Random number </button>
+    </div>
+  );
 };
 
 export default App;
