43ea3431a5dc8fa2eb41594bb34782e619129bd0
Rampravesh <rampraveshkr4545@gmail.com>
2026-02-14 15:25:41 +0530

    final submision of password generator

--- 19.Password Generator/src/App.jsx
+++ 19.Password Generator/src/App.jsx
@@ -1,20 +1,59 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const App = () => {
-  const [first, setfirst] = useState();
+  const [pasword, setpasword] = useState("");
+  const [length, setlength] = useState(6);
+  const [number, setnumber] = useState(false);
+  const [characters, setcharacters] = useState(false);
+  // const inpRef = useRef("");
+  const paswordGenerator = () => {
+    setpasword("");
+    let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numbers = "1234567890";
+    const character = '!"#$%&*+-.';
+    if (number) {
+      letters += numbers;
+    }
+    if (characters) {
+      letters += character;
+    }
+    for (let i = 0; i < length; i++) {
+      setpasword(
+        (prev) => prev + letters[Math.floor(Math.random() * letters.length)],
+      );
+    }
+  };
+  const handleRange = (e) => {
+    setlength(e.target.value);
+  };
+  useEffect(() => {
+    paswordGenerator();
+  }, [number, characters, length]);
+
+  useEffect(() => {
+    console.log("re renderd hua");
+  });
+
   return (
     <>
       <div>
-        <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+        <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500 select-none">
           <h1 className="text-white text-center my-3">Password Generator</h1>
           <div className="flex shadow rounded-lg overflow-hidden mb-4">
             <input
               type="text"
+              value={pasword}
               className="outline-none w-full py-1 px-3"
               placeholder="Password"
               readOnly
             />
-            <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+            <button
+              onClick={() => {
+                navigator.clipboard.writeText(pasword);
+                alert(`password ${pasword} copy`);
+              }}
+              className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 cursor-pointer"
+            >
               copy
             </button>
           </div>
@@ -23,22 +62,47 @@ const App = () => {
               <input
                 type="range"
                 min={6}
-                max={100}
+                max={20}
+                value={length}
+                onChange={handleRange}
                 className="cursor-pointer"
               />
-              <label>Length: 8</label>
+              <label>Length: {length}</label>
             </div>
             <div className="flex items-center gap-x-1">
-              <input type="checkbox" id="numberInput" />
+              <input
+                type="checkbox"
+                id="numberInput"
+                checked={number}
+                onChange={(e) => {
+                  setnumber(e.target.checked);
+                }}
+              />
               <label htmlFor="numberInput">Numbers</label>
             </div>
             <div className="flex items-center gap-x-1">
-              <input type="checkbox" id="characterInput" />
+              <input
+                type="checkbox"
+                id="characterInput"
+                checked={characters}
+                onChange={(e) => {
+                  setcharacters(e.target.checked);
+                }}
+              />
               <label htmlFor="characterInput">Characters</label>
             </div>
           </div>
           <div className="flex flex-col gap-2 mt-4">
-            <button className="outline-none rounded-2xl bg-blue-700 text-white px-3 py-0.5 shrink-0 cursor-pointer">
+            <button
+              onClick={() => {
+                setpasword("");
+                setlength(6);
+                setcharacters(false);
+                setnumber(false);
+                paswordGenerator();
+              }}
+              className="outline-none rounded-2xl bg-blue-700 text-white px-3 py-0.5 shrink-0 cursor-pointer"
+            >
               Reset Password
             </button>
             <button className="outline-none rounded-2xl bg-blue-700 text-white px-3 py-0.5 shrink-0 cursor-pointer">
