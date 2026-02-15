00079b4147d6364767ee1cc7aaa287a50f2e45ac
Rampravesh <rampraveshkr4545@gmail.com>
2026-02-15 15:55:54 +0530

    revision start

--- 16.Axios/src/App.jsx
+++ 16.Axios/src/App.jsx
@@ -1,16 +1,16 @@
 import axios from "axios";
 
 function App() {
-  // const fetchData = async () => {
-  //   const res = await fetch("http://jsonplaceholder.typicode.com/users");
-  //   const data = await res.json();
-  //   console.log(data);
-  // };
-
   const fetchData = async () => {
-    const data = await axios.get("http://jsonplaceholder.typicode.com/users");
+    const res = await fetch("http://jsonplaceholder.typicode.com/users",);
+    const data = await res.json();
     console.log(data);
   };
+
+  // const fetchData = async () => {
+  //   const data = await axios.get("http://jsonplaceholder.typicode.com/users");
+  //   console.log(data);
+  // };
   return (
     <>
       <h1>project setup</h1>
