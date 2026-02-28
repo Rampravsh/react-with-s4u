fcc8a158cc5fbb3134e14b714b50f1ecc6b23058
Rampravesh <rampraveshkr4545@gmail.com>
2026-02-28 18:33:42 +0530

    use Create Browser Router

--- 21.React Router Dom(II)/src/App.jsx
+++ 21.React Router Dom(II)/src/App.jsx
@@ -1,7 +1,11 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     <>
-      <h1>project setup</h1>
+      <Navbar />
+      <Outlet />
     </>
   );
 }
