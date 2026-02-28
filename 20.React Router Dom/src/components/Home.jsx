bc3450ff380b67a46884290e237de17447ad1bdd
Rampravesh <rampraveshkr4545@gmail.com>
2026-02-28 05:55:21 +0530

    NavLink and Link

--- 20.React Router Dom/src/components/Home.jsx
+++ 20.React Router Dom/src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <div className="page">Home</div>;
+  return <div className="page" id="home">Home</div>;
 };
 
 export default Home;
--- 20.React Router Dom/src/components/Navbar.jsx
+++ 20.React Router Dom/src/components/Navbar.jsx
@@ -1,14 +1,42 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
-      <nav className="flex justify-around w-full ">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/help">Help</Link>
+      <nav className="flex justify-around w-full bg-gray-800 p-4">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? "text-green-500" : "text-white"
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            isActive ? "text-green-500" : "text-white"
+          }
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/contact"
+          className={({ isActive }) =>
+            isActive ? "text-green-500" : "text-white"
+          }
+        >
+          Contact
+        </NavLink>
+        <NavLink
+          to="/help"
+          className={({ isActive }) =>
+            isActive ? "text-green-500" : "text-white"
+          }
+        >
+          Help
+        </NavLink>
       </nav>
     </>
   );
