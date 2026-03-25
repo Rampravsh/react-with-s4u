dc622658c2661e626fb42dfeb2140494417ce38f
Rampravesh <rampraveshkr4545@gmail.com>
2026-03-25 21:27:22 +0530

    axios install

--- 24.News App/src/App.jsx
+++ 24.News App/src/App.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import News from "./pages/News";
 import Category from "./components/Category";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
-      <Navbar />
-      <Category />
+      <div className="sticky top-0 z-10">
+        <Navbar />
+      </div>
+      <div className="sticky top-20 z-10">
+        <Category />
+      </div>
       <News />
+      <Footer />
     </>
   );
 };
--- 24.News App/src/components/Category.jsx
+++ 24.News App/src/components/Category.jsx
@@ -5,7 +5,7 @@ import { categories } from "../constant";
 const Category = () => {
   return (
     <Wrapper>
-      <div className="flex items-center md:justify-center px-2 gap-2 overflow-x-scroll md:overflow-hidden">
+      <div className="flex items-center md:justify-center mt-5 px-2 gap-2 overflow-x-scroll md:overflow-hidden">
         {categories.map((item) => (
           <button key={item} className="btn btn-neutral">
             {item}
